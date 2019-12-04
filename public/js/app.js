18353c797cb1c3112e53db0b49281ca7479f34f7
Jameson Aardalen <jamesonaardalen@gmail.com>
2019-12-04 05:24:34 -0800

    Setup app for Heroku deployment

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ weatherForm.addEventListener('submit', (e) => {
 
   messageOne.textContent = 'Loading...'
 
-  fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+  fetch('/weather?address=' + location).then((response) => {
     response.json().then(({
       location,
       forecast,
